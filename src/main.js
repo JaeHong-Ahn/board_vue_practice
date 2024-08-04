bbec031910c35dac23466aa388d5801aac526f00
JaeHongAhn <woghd387@kyonggi.ac.kr>
2024-08-04 15:37:07 +0900

    add axios

--- src/main.js
+++ src/main.js
@@ -3,5 +3,11 @@ import './assets/common.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import {routes} from "vue-router/auto-routes";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.config.globalProperties.$axios = axios;
+app.config.globalProperties.$serverUrl = '//localhost:8081';
+app.use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
